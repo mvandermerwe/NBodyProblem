70d2baab8653193130396c4b5cec991060d3c34b
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-01-22 17:03:26 -0700

    Filled in geometry vector methods.

--- NBODY/src/stars/Geometry_Vector.java
+++ NBODY/src/stars/Geometry_Vector.java
@@ -10,92 +10,96 @@ import java.awt.geom.Point2D;
  * @author germain
  *
  */
-public class Geometry_Vector extends Point2D.Double
-{
+public class Geometry_Vector extends Point2D.Double {
+	
+	private static final long serialVersionUID = 1L;
 
-  /**
-   *
-   * Constructor
-   *
-   * @param x - the X 
-   * @param y - and Y values
-   */
-  public  Geometry_Vector(double xx, double yy)
-  {
-	  
-  }
+	/**
+	 *
+	 * Constructor
+	 *
+	 * @param x
+	 *            - the X
+	 * @param y
+	 *            - and Y values
+	 */
+	public Geometry_Vector(double xx, double yy) {
+		this.x = xx;
+		this.y = yy;
+	}
 
-  /**
-   *
-   * A "Copy" constructor.  Creat ourself based on the given vector
-   * @param the_copy 
-   */
-   public  Geometry_Vector(Geometry_Vector the_copy)
-   {
-	   
-   }
+	/**
+	 *
+	 * A "Copy" constructor. Create ourself based on the given vector
+	 * 
+	 * @param the_copy
+	 */
+	public Geometry_Vector(Geometry_Vector the_copy) {
+		super(the_copy.getX(), the_copy.getY());
+	}
 
-  /**
-   * Add the components of the given vector to me.
-   *
-   * @param vector
-   */
-  public void add_to_me(Geometry_Vector vector)
-  {
-	  
-  }
+	/**
+	 * Add the components of the given vector to me.
+	 *
+	 * @param vector
+	 */
+	public void add_to_me(Geometry_Vector vector) {
+		this.x = this.getX() + vector.getX();
+		this.y = this.getY() + vector.getY();
+	}
 
-  /**
-   * Subtract the components of the given vector from me.
-   * @param vector
-   */
-  public void subtract_from_me(Geometry_Vector vector)
-  {
-	  
-  }
+	/**
+	 * Subtract the components of the given vector from me.
+	 * 
+	 * @param vector
+	 */
+	public void subtract_from_me(Geometry_Vector vector) {
+		this.x = this.getX() - vector.getX();
+		this.y = this.getY() - vector.getY();
+	}
 
-  /**
-   * Divide my components by the scalar
-   * @param scalar
-   */
-  public void divide_by( double scalar )
-  {
-	  
-  }
+	/**
+	 * Divide my components by the scalar
+	 * 
+	 * @param scalar
+	 */
+	public void divide_by(double scalar) {
+		this.x = this.getX() / scalar;
+		this.y = this.getY() / scalar;
+	}
 
-  /**
-   * Multiply my components by the scalar
-   * @param scalar
-   */
-  public  void   multiply_me_by(double scalar)
-  {
-	  
-  }
+	/**
+	 * Multiply my components by the scalar
+	 * 
+	 * @param scalar
+	 */
+	public void multiply_me_by(double scalar) {
+		this.x = this.getX() * scalar;
+		this.y = this.getY() * scalar;
+	}
 
-  /**
-   * @return my magnitude (the distance from the origin to my X,Y)
-   * Think Pythagoras
-   */
-  public  double  magnitude()
-  {
-	  return 0; // "FIXME";
-  }
+	/**
+	 * @return my magnitude (the distance from the origin to my X,Y) Think
+	 *         Pythagoras
+	 */
+	public double magnitude() {	
+		return Math.sqrt(Math.pow(this.getX(), 2) + Math.pow(this.getY(), 2));
+	}
 
-  /**
-   * @return an informative (but concise) description of this object
-   */
-  public  String toString()
-  {
-	  return "FIXME";
-  }
+	/**
+	 * @return an informative (but concise) description of this object
+	 */
+	public String toString() {
+		return "Vector: x=" + this.getX() + " y=" + this.getY() + " magnitude=" + this.magnitude();
+	}
 
-  /**
-   * Take this vector and turn it into a vector of length 1.  This is done by
-   * dividing each component (i.e., x,y)  by the magnitude.
-   */
-  public   void   normalize()
-  {
-	  
-  }
-  
+	/**
+	 * Take this vector and turn it into a vector of length 1. This is done by
+	 * dividing each component (i.e., x,y) by the magnitude.
+	 */
+	public void normalize() {
+		this.x = this.getX() / this.magnitude();
+		this.y = this.getY() / this.magnitude();
+	}
+
 }
